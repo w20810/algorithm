99296e10799e637cf85f03ada69ab72e119cd20c
yuxing <376365042@qq.com>
2016-12-05 19:08:12 +0800

    添加push_heap, pop_heap, make_heap, sort_heap算法(未测试)

--- imitate_stl/algorithm.h
+++ imitate_stl/algorithm.h
@@ -51,6 +51,85 @@ namespace yxSTL
 				break;
 		return cur;	
 	}
+
+	template <typename RandomAccessIterator>
+	void push_heap(RandomAccessIterator first, RandomAccessIterator last)
+	{
+		__push_heap(first, last - first - 1, *(last - 1));
+	}
+	
+	template <typename RandomAccessIterator, typename Distance, typename T>
+	void __push_heap(RandomAccessIterator first, Distance holeIndex, T value)
+	{
+		Distance parentIndex = (holeIndex - 1) / 2;
+		while (holeIndex != 0 && *(first + parentIndex) < value)
+		{
+			*(first + holeIndex) = *(first + parentIndex);
+			holeIndex = parentIndex;
+			parentIndex = (holeIndex - 1) / 2;
+		}
+		*(first + holeIndex) = value;
+	}
+
+	template <typename RandomAccessIterator>
+	void pop_heap(RandomAccessIterator first, RandomAccessIterator last)
+	{
+		*first = *(last - 1);
+		__adjust_heap(first, last - first - 1, first - first, *first);
+	}
+
+	template <typename RandomAccessIterator, typename Distance, typename T>
+	void __adjust_heap(RandomAccessIterator first, Distance len, Distance holeIndex, T value)//从holeIndex处向下调整
+	{
+		Distance rSonIndex = holeIndex * 2 + 2;
+		while (rSonIndex < len)
+		{
+			if (*(first + rSonIndex) < *(first + (rSonIndex - 1)))
+				--rSonIndex;
+			if (value < *(first + rSonIndex))
+			{
+				*(first + holeIndex) = *(first + rSonIndex);
+				holeIndex = rSonIndex;
+				rSonIndex = holeIndex * 2 + 2;
+			}
+			else
+				break;
+		}
+		if (rSonIndex - 1 == len)
+		{
+			if (value < *(first + (rSonIndex - 1)))
+			{
+				*(first + holeIndex) = *(first + (rSonIndex - 1));
+				holeIndex = rSonIndex - 1;
+			}
+		}
+		*(first + holeIndex) = value;
+	}
+
+	template <typename RandomAccessIterator>
+	void sort_heap(RandomAccessIterator first, RandomAccessIterator last)
+	{
+		while (first - last > 1)
+			pop_heap(first, last--);
+	}
+
+	template <typename RandomAccessIterator>
+	void make_heap(RandomAccessIterator first, RandomAccessIterator last)
+	{
+		__make_heap(first, last - first);
+	}
+
+	template <typename RandomAccessIterator, typename Distance>
+	void __make_heap(RandomAccessIterator first, Distance len)
+	{	
+		Distance curIndex = (len - 2) / 2;
+		while (curIndex >= 0)
+		{
+			__adjust_heap(first, len, curIndex, *(first + curIndex));
+			--curIndex;
+		}
+	}
+
 }//end yxSTL
 
 #endif      
